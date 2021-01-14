1ca8da721b1b2aa30b59065be140f61671341d9b
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-14 15:01:28 -0600

    added the ability to cycle pick characters from each list at random

--- script.js
+++ script.js
@@ -3,40 +3,63 @@
 // Starting Data ==============================
 
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// list of numbers, characters, and special characters
-const numberList = '0123456789';
-const uppercaseList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowercaseList = 'abcdefghijklmnopqrstuvwxyz';
-const specialCharactersList = ' !"#$%&()*+,~./:;<>=?@[]^_`{}|-\'\\';
+// build a character set containing the users choices
+const numberList = "0123456789";
+const uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+const specialCharactersList = " !\"#$%&()*+,~./:;<>=?@[]^_`{}|-'\\";
 
 // User Inputs ================================
+
 function getParameters() {
-// ask the user how long they want the password
-let passwordLength = prompt("How long would you like your password to be? (Min. 8 Characters)")
+  // ask the user how long they want the password
+  let passwordLength = prompt(
+    "How long would you like your password to be? (Min. 8 Characters)"
+  );
 
-// do they want SVGAnimatedNumberList
-let includeNumbers = confirm('Would you like to include numbers in your password?')
+  // do they want SVGAnimatedNumberList
+  let includeNumbers = confirm(
+    "Would you like to include numbers in your password?"
+  );
 
-// do they want lowercase letters
-let includeLowerCaseLetters = confirm('Would you like to include lowercase letters in your password?')
+  // do they want lowercase letters
+  let includeLowerCaseLetters = confirm(
+    "Would you like to include lowercase letters in your password?"
+  );
 
-// do they want uppercase letters
-let includeUpperCaseLetters = confirm('Would you like to include uppercase letters in your password?')
+  // do they want uppercase letters
+  let includeUpperCaseLetters = confirm(
+    "Would you like to include uppercase letters in your password?"
+  );
 
-//do they want special characters?
-let includeSpecialCharacters = confirm('Would you like to include special characters in your password?')
+  //do they want special characters?
+  let includeSpecialCharacters = confirm(
+    "Would you like to include special characters in your password?"
+  );
 }
 
-
 // Functions ==================================
+function generatePassword() {
+  let numChoice = numberList.charAt(
+    Math.floor(Math.random() * numberList.length)
+  );
+  let uppercaseChoice = uppercaseList.charAt(
+    Math.floor(Math.random() * uppercaseList.length)
+  );
+  let lowercaseChoice = lowercaseList.charAt(
+    Math.floor(Math.random() * lowercaseList.length)
+  );
+  let specialChoice = specialCharactersList.charAt(
+    Math.floor(Math.random() * specialCharactersList.length)
+  );
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword() {
-
-  };
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -44,16 +67,17 @@ function writePassword() {
   passwordText.placeholder = passwordText.value;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 var passwordText = document.querySelector("#password");
 
+// Initialize =====================================
+// getParameters();
 
-
-// build a character set containing the users choices
+// to do ==========================================
 // randomly grab characters from the character set
+//let compChoice = compChoices.charAt(Math.floor(Math.random() * 3));
 // place the random characters in the password until we have a password at the specified length
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
 
-// Initialize (run the function that starts everything)
-getParameters();
+// debugging =======================================
+generatePassword();
